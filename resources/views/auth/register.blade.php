b0220c7d7744c41e0e650c912e0589e9662bfde4
it <s073009.git@gmail.com>
2019-07-06 08:06:54 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,32 @@
+@include('layouts.app')
+
+@section('content')
+
+   <div class="text-center">
+       <h1>Signup</h1>
+   </div>
+   
+   {!! Form::open(['route' => 'signup.post']) !!}
+       <div class="Form-group">
+           {!! Form::label('name' , 'Name') !!}
+           {!! Form::text('name' , old('name') , ['class' => 'form-control']) !!}
+       </div>
+       
+       <div class="Form-group">
+           {!! Form::label('email' , 'Email') !!}
+           {!! Form::email('email' , old('email') , ['class' => 'form-control']) !!}
+       </div>
+       
+       <div class="Form-group">
+           {!! Form::label('password' , 'Password') !!}
+           {!! Form::password('password' , ['class' => 'form-control']) !!}
+       </div>
+       
+       <div class="Form-group">
+           {!! Form::label('password_confirmation' , 'confirmation') !!}
+           {!! Form::password('password_confirmation' , ['class' => 'form-control']) !!}
+       </div>
+       
+       {!! Form::submit('Sign up' , ['class' => 'btn btn-primary btn-block']) !!}
+    {!! Form::close() !!}
+@endsection
